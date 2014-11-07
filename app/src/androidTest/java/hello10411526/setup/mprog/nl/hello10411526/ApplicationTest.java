0afc38ae526a4b869b3a54d61776af9549a08cff
emMusti Aksoy/em <emaksoy.m.h@gmail.com/em>
2014-11-07 11:51:02 +0100

    Hoop dat het lukt

---
+++ app/src/androidTest/java/hello10411526/setup/mprog/nl/hello10411526/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hello10411526.setup.mprog.nl.hello10411526;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
